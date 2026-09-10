3e4053223927e30c0cddc62bbf6e2271ac020a63
abhinav <abhinavpadige06@gmail.com>
2026-09-11 01:01:52 +0530

    Update Next.js config for MDX and SVG support

--- next.config.js
+++ next.config.js
@@ -3,9 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['avatars.githubusercontent.com', 'github.com'],
+    domains: ['avatars.githubusercontent.com', 'github.com', 'images.unsplash.com'],
   },
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
